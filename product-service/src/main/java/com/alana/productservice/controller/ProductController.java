e1654dbaabdd9dba7e5d8982e86465631096b692
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-04 01:37:45 +0100

    postmapping all done

--- product-service/src/main/java/com/alana/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/alana/productservice/controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/api/product")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
     public ProductController(ProductService productService) {
--- product-service/src/main/java/com/alana/productservice/service/ProductService.java
+++ product-service/src/main/java/com/alana/productservice/service/ProductService.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
