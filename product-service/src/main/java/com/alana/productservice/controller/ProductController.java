6184298a892eb5d5360642161c333826383b3ba8
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-04 01:04:38 +0100

    services

--- product-service/src/main/java/com/alana/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/alana/productservice/controller/ProductController.java
@@ -6,26 +6,23 @@ import com.alana.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
-
     private final ProductService productService;
-
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
     }
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
-
 }
--- product-service/src/main/java/com/alana/productservice/service/ProductService.java
+++ product-service/src/main/java/com/alana/productservice/service/ProductService.java
@@ -1,39 +1,34 @@
-package com.programmingtechie.productservice.service;
+package com.alana.productservice.service;
 
-import com.programmingtechie.productservice.dto.ProductRequest;
-import com.programmingtechie.productservice.dto.ProductResponse;
-import com.programmingtechie.productservice.model.Product;
-import com.programmingtechie.productservice.repository.ProductRepository;
+import com.alana.productservice.dto.ProductRequest;
+import com.alana.productservice.dto.ProductResponse;
+import com.alana.productservice.model.Product;
+import com.alana.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
-
     private final ProductRepository productRepository;
-
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
     public void createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
                 .build();
-
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
     }
-
     public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
-
         return products.stream().map(this::mapToProductResponse).toList();
     }
-
     private ProductResponse mapToProductResponse(Product product) {
         return ProductResponse.builder()
                 .id(product.getId())
