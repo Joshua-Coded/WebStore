6747853df2ee804d84e59fd5310dfb4488ddfa75
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-04 03:38:56 +0100

    inventory service

---
+++ inventory-service/src/test/java/com/alana/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alana.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
